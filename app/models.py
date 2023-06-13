e3a9dcbc0e6043ea464702fc2dedad9e88d0f294
chiragjikadra <87493029+chiragjikadra@users.noreply.github.com>
2023-06-13 11:13:00 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,51 +1,22 @@
-from PIL.Image import Image
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 
-#  Custom User Manager
-class UserManager(BaseUserManager):
-    def create_user(self, email, username, password=None, password2=None):
-        """
-      Creates and saves a User with the given email, username and password.
-      """
-        if not email:
-            raise ValueError('User must have an email address')
-
-        user = self.model(
-            email=self.normalize_email(email),
-            username=username
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-
-#  Custom User Model
-class User(AbstractBaseUser):
-    email = models.EmailField(verbose_name='Email', max_length=255, unique=True)
-    username = models.CharField(max_length=200)
-
-    objects = UserManager()
+class User(AbstractUser):
+    username = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
-    def __str__(self):
-        return self.username
+    REQUIRED_FIELDS = []
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_pic = models.ImageField(upload_to='profile_photos')
+    photo = models.ImageField(upload_to='profile_photos', blank=True, null=True)
     designation = models.CharField(max_length=100)
+    other_details = models.TextField(blank=True)
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        img = Image.open(self.profile_photo.path)
-        if img.size[0] > 1000 or img.size[1] > 1000:
-            output_size = (1000, 1000)
-            img.thumbnail(output_size)
-            img.save(self.profile_photo.path)
